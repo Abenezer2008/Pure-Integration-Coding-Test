bf1a27865434b6891ba85fdd08a04e4169f76d31
abenezer2008 <abenezersintayehu22@gmail.com>
2021-09-24 16:25:52 -0500

    Add unit test for Repository class

--- src/test/java/com/pureintegration/test/repository/TestDogBreedRepository.java
+++ src/test/java/com/pureintegration/test/repository/TestDogBreedRepository.java
@@ -1,4 +1,29 @@
 package com.pureintegration.test.repository;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 public class TestDogBreedRepository {
+    private DogBreedRepository dogBreedRepository;
+
+    @Autowired
+    public TestDogBreedRepository(DogBreedRepository dogBreedRepository){
+        this.dogBreedRepository = dogBreedRepository;
+    }
+
+    @Test
+    public void shouldPass(){
+        Map<String, List<String>> response = dogBreedRepository.getDogBreedMap();
+
+        assertNotNull(response);
+        assertEquals(response.get("waterdog"),List.of("spanish"));
+    }
 }
