2056e2a4d0b75a661897d16850370f0f394042a4
abenezer2008 <abenezersintayehu22@gmail.com>
2021-09-24 15:54:13 -0500

    Override equal and hashCode method to compare by value

--- src/main/java/com/pureintegration/test/dto/DogBreedDTO.java
+++ src/main/java/com/pureintegration/test/dto/DogBreedDTO.java
@@ -2,6 +2,7 @@ package com.pureintegration.test.dto;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 public class DogBreedDTO {
     private Map<String, Object> dogBreedResponse;
@@ -14,5 +15,18 @@ public class DogBreedDTO {
 
     public Map<String, Object> getDogBreedResponse() {
         return dogBreedResponse;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        DogBreedDTO that = (DogBreedDTO) o;
+        return Objects.equals(dogBreedResponse, that.dogBreedResponse);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(dogBreedResponse);
     }
 }
